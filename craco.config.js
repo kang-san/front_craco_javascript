0a47101dd93e7f171c27f23be17b0f98ca6f5acc
kang-san <junbuck01@naver.com>
2023-02-02 18:24:06 +0900

    build craco project

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+
+    plugins: [
+        {
+            plugin: CracoAlias,
+            options: {
+                source: 'jsconfig',
+                jsConfigPath: 'jsconfig.paths.json',
+            },
+        },
+    ],
+};
---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from "../screen/home";
+
+
+export default function App() {
+  return (
+      <BrowserRouter basename='/'>
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
